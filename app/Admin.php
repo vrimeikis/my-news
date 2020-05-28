cc577ce6977c136b70682fa18d86adf98b0f6ef6
vytautas <vytautas.rimeikis@gmail.com>
2020-05-28 21:50:50 +0300

    models phpdocs

--- app/Admin.php
+++ app/Admin.php
@@ -5,12 +5,41 @@ declare(strict_types = 1);
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
 /**
  * Class Admin
+ *
  * @package App
+ * @property int $id
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static Builder|Admin newModelQuery()
+ * @method static Builder|Admin newQuery()
+ * @method static Builder|Admin query()
+ * @method static Builder|Admin whereCreatedAt($value)
+ * @method static Builder|Admin whereEmail($value)
+ * @method static Builder|Admin whereEmailVerifiedAt($value)
+ * @method static Builder|Admin whereFirstName($value)
+ * @method static Builder|Admin whereId($value)
+ * @method static Builder|Admin whereLastName($value)
+ * @method static Builder|Admin wherePassword($value)
+ * @method static Builder|Admin whereRememberToken($value)
+ * @method static Builder|Admin whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Admin extends Authenticatable
 {
