017d4aba689b13a0db66c50bea3770f5e432f8e8
vytautas <vytautas.rimeikis@gmail.com>
2020-06-01 21:49:05 +0300

    admin layout

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Dashboard</div>
+
+        <div class="card-body">
+            You are logged in as admin!
+        </div>
+    </div>
+@endsection
